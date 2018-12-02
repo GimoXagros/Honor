7fd9db6b75f00b2c056703fbf06b3d29aece248b
BaeSungJin <baehun92@naver.com>
2018-12-03 00:37:28 +0900

    TeamBatttlePlayerController 추가.
    TeamBattle 모드에서 사용자가 입력을 처리할 플레이어 컨트롤러추가

---
+++ Source/Honor/TeamBattle/TeamBattlePlayerController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TeamBattlePlayerController.h"
+
+
+
+ATeamBattlePlayerController::ATeamBattlePlayerController() {
+
+}
+
+
+void ATeamBattlePlayerController::BeginPlay() {
+  Super::BeginPlay();
+
+}
